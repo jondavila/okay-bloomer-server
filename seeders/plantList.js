f3ea2d422c10c4323696b98a9e1e14fef09412d8
badoodee95 <monzoncaleb@yahoo.com>
2023-07-12 21:13:42 -0700

    feat: seeded plant lists to local database

--- seeders/plantList.js
+++ seeders/plantList.js
@@ -1,2 +1,12 @@
+const PlantList = require('../models/plantList');
+const { addPlantList } = require('../utils');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/plant-app', { useNewUrlParser: true, useUnifiedTopology: true });
+
+addPlantList(1);
+
+for (let i = 2; i <= 10; i++) {
+    addPlantList(i);
+}
 
