e5d300c5f2099ee1332b961a7d185b950bd59699
badoodee95 <monzoncaleb@yahoo.com>
2023-07-12 22:29:09 -0700

    feat: add GET / route for controllers/plantLists.js

---
+++ controllers/plantLists.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// import the plantList model
+const PlantList = require('../models/plantList');
+
+router.get('/', function (req, res) {
+    PlantList.find({})
+        .then((plantLists) => {
+            console.log('plantLists', plantLists);
+            res.json({ plantLists: plantLists });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+
+module.exports = router;
