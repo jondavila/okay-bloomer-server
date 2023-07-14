54d22b321275e3869715335c829f9d4a0478564e
badoodee95 <monzoncaleb@yahoo.com>
2023-07-14 10:12:17 -0700

    feat: created /plantDetails/:plantId route

--- controllers/plantDetails.js
+++ controllers/plantDetails.js
@@ -22,9 +22,43 @@ router.get('/type/:plantType', function (req, res) {
         .then((plantDetails) => {
             console.log('plantDetails', plantDetails);
             res.json({ plantDetails: plantDetails });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
         });
-
 });
+
+router.get('/plantTypes', function (req, res) {
+    PlantDetail.find({})
+        .then((plantDetails) => {
+            let result = [];
+            for (let i = 0; i < plantDetails.length; i++) {
+                if (!result.includes(plantDetails[i].type)) {
+                    result.push(plantDetails[i].type);
+                }
+            }
+            console.log('plantTypes', result);
+            res.json({ plantTypes: result });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+router.get('/:plantId', function (req, res) {
+    PlantDetail.findOne({ plantId: req.params.plantId })
+        .then((plantDetail) => {
+            console.log('plantDetail', plantDetail);
+            res.json({ plantDetail: plantDetail });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
 
 module.exports = router;
 
