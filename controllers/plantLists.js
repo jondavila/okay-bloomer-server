dcc15b2b0a575dde162eb105f861c6c0f0c6d126
badoodee95 <monzoncaleb@yahoo.com>
2023-07-15 10:33:37 -0700

    feat: created more routes for /plantLists

--- controllers/plantLists.js
+++ controllers/plantLists.js
@@ -28,6 +28,103 @@ router.get('/:plantId', function (req, res) {
         });
 });
 
+router.get('/journalEntries/:plantId', function (req, res) {
+    PlantList.findOne({ plantId: req.params.plantId })
+        .then((plantList) => {
+            console.log('plantList', plantList);
+            res.json({ journalEntries: plantList.journalEntries });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+router.get('/journalEntries/:plantId', function (req, res) {
+    PlantList.findOne({ plantId: req.params.plantId })
+        .then((plantList) => {
+            console.log('plantList', plantList);
+            res.json({ journalEntries: plantList.journalEntries });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+// ================================ POST ================================ //
+
+router.post('/new', function (req, res) {
+    PlantList.findOne({ plantId: req.params.plantId })
+        .then((plantList) => {
+            plantList.journalEntries.push(req.body);
+            plantList.save()
+                .then((plantList) => {
+                    res.json({ journalEntries: plantList.journalEntries });
+                })
+                .catch((error) => {
+                    console.log('error', error);
+                    res.json({ message: 'There was an issue, please try again...' });
+                });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+
+// ================================ PUT ================================ //
+
+router.put('/journalEntries/:plantId/:journalEntryId', function (req, res) {
+    PlantList.findOne({ plantId: req.params.plantId })
+        .then((plantList) => {
+            plantList.journalEntries.id(req.params.journalEntryId).entry = req.body.entry;
+            plantList.save()
+                .then((plantList) => {
+                    res.json({ journalEntries: plantList.journalEntries });
+                })
+                .catch((error) => {
+                    console.log('error', error);
+                    res.json({ message: 'There was an issue, please try again...' });
+                });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+
+// ================================ DELETE ================================ //
+
+router.delete('/journalEntries/:plantId/:journalEntryId', function (req, res) {
+    PlantList.findOne({ plantId: req.params.plantId })
+        .then((plantList) => {
+            plantList.journalEntries.id(req.params.journalEntryId).remove();
+            plantList.save()
+                .then((plantList) => {
+                    res.json({ journalEntries: plantList.journalEntries });
+                })
+                .catch((error) => {
+                    console.log('error', error);
+                    res.json({ message: 'There was an issue, please try again...' });
+                });
+        })
+        .catch((error) => {
+            console.log('error', error);
+            res.json({ message: 'There was an issue, please try again...' });
+        });
+});
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
