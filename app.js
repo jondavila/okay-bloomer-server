15fde9a05ffa008a2fa9235ffdeb0a42cc56741f
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-07-19 21:13:11 -0700

    fix: fix /controllers/plantdetails.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/posts', require('./controllers/posts'));
 app.use('/users', require('./controllers/users'));
-app.use('/plantdetails', require('./controllers/plantdetails'));
+app.use('/plantdetails', require('./controllers/plantDetails'));
 app.use('/plantguides', require('./controllers/plantguides'));
 app.use('/plantfaqs', require('./controllers/plantfaqs'));
 app.use('/sanctuary', require('./controllers/sanctuary'));
